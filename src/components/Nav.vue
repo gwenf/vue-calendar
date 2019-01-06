ce1275e57e576b7042f93e270954f3a0af943b20
gwenf <gwenfaraday@gmail.com>
2019-01-06 08:48:09 -0500

    first commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item to="/">Home</b-nav-item>
+      <b-nav-item to="about">About</b-nav-item>
+      <b-nav-item to="calendar">Calendar</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+// https://bootstrap-vue.js.org/docs/reference/router-links/
+</script>
---
+++ src/views/Calendar.vue
@@ -0,0 +1,13 @@
+<template>
+  <vue-cal style="height: 750px"></vue-cal>
+</template>
+
+<script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+
+export default {
+  components: { VueCal }
+}
+</script>
+
